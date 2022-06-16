92079f46c27da57b3eee90f0c4b2d480b14edfb4
Mario <yzarckwow@gmail.com>
2022-06-16 12:59:00 -0300

    update function onmouse and trasition

--- main.js
+++ main.js
@@ -6,15 +6,77 @@ const amazonas = document.getElementById("state-am");
 const para = document.getElementById("state-pa");
 const rioGrandeDoSul = document.getElementById("state-rs");
 
+let arrayStates = []
+arrayStates.push(acre, amazonas, espiritoSanto, para, rioDeJaneiro, rioGrandeDoSul, saoPaulo)
+
 const image = document.getElementById("img");
 
+
+
 //----------------------------- ACRE --------------------------------//
-function mouseOverAcre() {
-    image.classList.add("acre-hover");
+function mouseOverActiveImg() {
+    if(event.target.id === 'state-ac') {
+        image.classList.add("acre-hover");
+        return;
+    }
+    if(event.target.id === 'state-am') {
+        image.classList.add("amazonas-hover");
+        return;
+    }
+    if(event.target.id === 'state-es') {
+        image.classList.add("espirito-s-hover");
+        return;
+    }
+    if(event.target.id === 'state-pa') {
+        image.classList.add("para-hover");
+        return;
+    }
+    if(event.target.id === 'state-rj') {
+        image.classList.add("rio-hover");
+        return;
+    }
+    if(event.target.id === 'state-rs') {
+        image.classList.add("rio-g-sul-hover");
+        return;
+    }
+    if(event.target.id === 'state-sp') {
+        image.classList.add("sao-paulo-hover");
+        return;
+    }
+    
+    
 }
 
-function mouseOutAcre() {
-    image.classList.remove("acre-hover");
+function mouseOutDisablesImg() {
+    if(event.target.id === 'state-ac') {
+        image.classList.remove("acre-hover");
+        return;
+    }
+    if(event.target.id === 'state-am') {
+        image.classList.remove("amazonas-hover");
+        return;
+    }
+    if(event.target.id === 'state-es') {
+        image.classList.remove("espirito-s-hover");
+        return;
+    }
+    if(event.target.id === 'state-pa') {
+        image.classList.remove("para-hover");
+        return;
+    }
+    if(event.target.id === 'state-rj') {
+        image.classList.remove("rio-hover");
+        return;
+    }
+    if(event.target.id === 'state-rs') {
+        image.classList.remove("rio-g-sul-hover");
+        return;
+    }
+    if(event.target.id === 'state-sp') {
+        image.classList.remove("sao-paulo-hover");
+        return;
+    }
+    
 }
 
 //----------------------------- RIO DE JANEIRO --------------------------------//
